d234391f2eddefaa69aca69dc4684e204a5f9622
Krzysztof Bociurko <git@chanibal.pl>
2017-05-08 14:01:21 +0200

    Fix redmine-517: log listener issues

---
+++ Logger/TraceLogListener.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Bivrost.Log
+{
+	/// <summary>
+	/// Writing to the console
+	/// </summary>
+	public class TraceLogListener : LogListener
+	{
+		private bool messageOnly;
+
+		public TraceLogListener(bool messageOnly)
+		{
+			this.messageOnly = messageOnly;
+		}
+
+		public void Write(Logger.LogElement entry)
+		{
+			if (messageOnly)
+			{
+				Trace.WriteLine(entry.msg);
+			}
+			else
+			{
+				Trace.WriteLine($"{entry.time} at {entry.path} {entry.type.ToString()}");
+				Trace.Indent();
+				Trace.WriteLine(entry.msg);
+				Trace.Unindent();
+				Trace.WriteLine("");
+			}
+			Trace.Flush();
+		}
+	}
+}
---
+++ Logger/WindowsEventLogListener.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Bivrost.Log
+{
+	/// <summary>
+	/// Windows Event Log Writer.
+	/// </summary>
+	public class WindowsEventLogListener : LogListener
+	{
+
+		string sSource;
+
+		static EventLogEntryType ToEventLogEntryType(LogType t)
+		{
+			switch (t)
+			{
+				default:
+				case LogType.info:
+				case LogType.notification:
+					return EventLogEntryType.Information;
+				case LogType.error:
+					return EventLogEntryType.Warning;
+				case LogType.fatal:
+					return EventLogEntryType.Error;
+			}
+		}
+
+
+		/// <summary>
+		/// Logger with custom application name. This requires admin rights.
+		/// </summary>
+		/// <param name="appname"></param>
+		/// <param name="logname"></param>
+		public WindowsEventLogListener(string appname, string logname)
+		{
+			if (!EventLog.SourceExists(sSource))
+				EventLog.CreateEventSource(sSource, logname);
+			sSource = appname;
+		}
+
+
+		/// <summary>
+		/// Logger with generic "Application" application name.
+		/// </summary>
+		public WindowsEventLogListener()
+		{
+			sSource = "Application";
+		}
+
+
+		public void Write(Logger.LogElement entry)
+		{
+			EventLog.WriteEntry(sSource, entry.type + ": " + entry.msg, ToEventLogEntryType(entry.type));
+		}
+
+	}
+
+}
