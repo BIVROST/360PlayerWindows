128b204e8d0f990148b8a566c6ad82a493f0887f
Krzysztof Bociurko <git@chanibal.pl>
2017-05-09 20:44:08 +0200

    GhostVR mock update

--- Docs/GhostVR-api-mock/index.js
+++ Docs/GhostVR-api-mock/index.js
@@ -7,25 +7,30 @@ app.use(bodyParser.json());
 
 var Token = {
 	tokens : {},
+	STATUS_OK : "ok",
+	STATUS_REJECTED: "rejected",
+	STATUS_PENDING: "pending",
 	status: function(token) {
 		if(!(token in this.tokens))
-			return "pending";
+			return Token.STATUS_PENDING;
 		return this.tokens[token];
 	},
 	reject: function(token) {
-		this.tokens[token] = "rejected";
+		if(!this.status(token) == Token.STATUS_REJECTED)
+			throw "token must already rejected";
+		this.tokens[token] = Token.STATUS_REJECTED;
 	},
 	accept: function(token) {
-		if(!this.status(token) == "pending")
+		if(!this.status(token) == Token.STATUS_PENDING)
 			throw "token must be pending to accept";
-		this.tokens[token] = "ok";
+		this.tokens[token] = Token.STATUS_OK;
 	}
 }
 
 function extractToken(req) {
 	var auth = req.header("Authorization");
 	if(auth) {
-		let token = auth.split(" ")[1];
+		var token = auth.split(" ")[1];
 		log("token from header: " + token)
 		return token;
 	}
@@ -36,19 +41,49 @@ function log() {
 	console.log([].slice.call(arguments).map(function(o) { return (o.substr)?o:JSON.stringify(o, undefined, 2); }).join(" "));
 }
 
+function requireValidTokenJsend(req,res,next) {
+	var token = extractToken(req);
+	console.log("jsend require valid token, token=" + token + " status=" + Token.status(token));
+	if(Token.status(token) != Token.STATUS_OK) {
+		console.log("   rejected");
+		res.jerror("token not ok");
+	}
+	else {
+		console.log("   accepted");
+		next();
+	}
+}
+
+function requireValidTokenHtml(req,res,next) {
+	var token = extractToken(req);
+	console.log("html require valid token, token=" + token + " status=" + Token.status(token));
+	if(Token.status(token) != Token.STATUS_OK) {
+		console.log("   rejected");
+		res.status(401);
+		res.send("token not ok");
+	}
+	else {
+		console.log("   accepted");
+		next();
+	}
+}
+
 app.get("/", function (req, res) {
-	res.send(JSON.stringify(Token.tokens));
+	res.send(Object.keys(Token.tokens).map(function(t){ 
+		var st=Token.status(t); 
+		return '<p>' + t + ' (' + st + ') <a href="/mock/reject/' + t +'">reject</a> <a href="/mock/accept/' + t +'">accept</a></p>' ;
+	}).join("\n"));
 });
 
-app.get("/mock/accept/:token", function(req,res) {
-	log("mock:accept("+req.params.token+")");
-	Token.accept(req.params.token);
+app.get("/mock/accept/:access_token", function(req,res) {
+	log("mock:accept("+req.params.access_token+")");
+	Token.accept(req.params.access_token);
 	res.send("OK");
 });
 
-app.get("/mock/reject/:token", function(req,res) {
-	log("mock:reject("+req.params.token+")");
-	Token.reject(req.params.token);
+app.get("/mock/reject/:access_token", requireValidTokenHtml, function(req,res) {
+	log("mock:reject("+req.params.access_token+")");
+	Token.reject(req.params.access_token);
 	res.send("OK");
 });
 
@@ -65,42 +100,46 @@ app.all("/mock/test", function(req,res) {
 	res.jsend({mock:true, info:info});
 });
 
-app.get("/mock/follow_up/:video_id", function(req,res) {
+app.get("/moxck/follow_up/:video_id", function(req,res) {
 	log("mock:follow_up");
-	var info = {
-		body:req.body,
-		params:req.params,
-		headers:req.headers,
-		token:req.token
-	};
-	log(info);
 	res.send(
 		"FOLLOW UP for "+req.params.video_id
-		+'<button onclick="window.external.ActionCompleted()">complete</button>'
-		+'<button onclick="window.external.ActionCanceled()">cancel</button>'
+		+'\n<a href="/mock/follow_up/'+req.params.video_id+'/completed">completed</a>'
+		+'\n<a href="/mock/follow_up/'+req.params.video_id+'/canceled">canceled</a>'
+		+'\n<hr /><button onclick="errrr()">js error</button>'
+		+'\n<button onclick="(function(xhr) { xhr.open(\'get\', \'/xherrrr\'); xhr.send(); } )(new XMLHttpRequest());">xhr error local</button>'
+		+'\n<button onclick="(function(xhr) { xhr.open(\'get\', \'http://xherrrr.com\'); xhr.send(); } )(new XMLHttpRequest());">xhr error nxdomain</button>'
+		+'\n<a href="http://errrrrrrrrrrrrrrrrr.com">navigation error nxdomain</a>'
+		+'\n<a href="/errrr">navigation error 404</a>'
 	);
+});
+
+app.get("/mock/follow_up/:video_id/:decision", function(req,res) {
+	log("mock:follow_up decision " + req.params.decision);
+	switch(req.params.decision) {
+		case "completed":
+			res.send('<script type="text/javascript">window.external.ActionCompleted()</script>');
+			break;
+		case "canceled":
+			res.send('<script type="text/javascript">window.external.ActionCompleted()</script>');
+			break;
+		default:
+			throw "decision error";
+	}
 });
 
 app.get("/api/v1/authorize_player", function(req,res) {
+	console.log(req);
 	var token=extractToken(req);
-	var installation_id=req.params.installation_id;
-	var player=req.params.player;
+	var installation_id=req.query.installation_id;
+	var player=req.query	.player;
 	log("authorize_player(" + token + ")");
 	res.send(
 		JSON.stringify({token:token, installation_id:installation_id, player:player})
-		+'<br/><a href="/mock/accept/'+token+'">accept</a>'
-		+'/ <a href="/mock/reject/'+token+'">reject</a>'
+		+' <a href="/mock/accept/'+token+'">accept</a>'
+		+' <a href="/mock/reject/'+token+'">reject</a>'
 	);
 });
-
-function requireValidToken(req,res,next) {
-	var token = extractToken(req);
-	console.log("require valid token, token = " + token + " status=" + Token.status(token));
-	if(Token.status(token) != "ok")
-		res.jerror("token not ok");
-	else
-		next();
-}
 
 app.post("/api/v1/verify_player_token", function(req,res) {
 	var token=extractToken(req);
@@ -108,14 +147,14 @@ app.post("/api/v1/verify_player_token", function(req,res) {
 	res.jsend({ "verification_status": Token.status(token), "team_name": "mock" })
 });
 
-app.post("/api/v1/discard_player_token", requireValidToken, function(req,res) {
+app.post("/api/v1/discard_player_token", function(req,res) {
 	var token=extractToken(req);
 	log("discard_player_token(" + token + ")");
 	Token.reject(token);
 	res.send("OK");
 });
 
-app.post("/api/v1/video_session", requireValidToken, function(req,res) {
+app.post("/api/v1/video_session", requireValidTokenJsend, function(req,res) {
 	var token=extractToken(req);
 	log("video_session");
 	res.jsend({ follow_up: "http://localhost:3000/mock/follow_up/" + req.body.guid });
