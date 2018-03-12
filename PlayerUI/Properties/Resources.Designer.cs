606ecd51b4a9807b58dcd6f245d0ed81f3543482
Krzysztof Bociurko <git@chanibal.pl>
2018-03-12 14:41:41 +0100

    Fixed the namespace in the default project settings and resources

--- PlayerUI/Properties/Resources.Designer.cs
+++ PlayerUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Bivrost360Player.Properties {
+namespace Bivrost.Bivrost360Player.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Bivrost360Player.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Bivrost360Player.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Bivrost.Bivrost360Player.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
