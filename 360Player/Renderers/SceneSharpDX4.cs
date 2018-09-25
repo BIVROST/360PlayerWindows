795c6826fe326921015f7ffbaf8a6c5097396882
Krzysztof Bociurko <git@chanibal.pl>
2018-09-25 18:50:01 +0200

    DirectX Textures

--- 360Player/Renderers/SceneSharpDX4.cs
+++ 360Player/Renderers/SceneSharpDX4.cs
@@ -11,17 +11,18 @@ using assFlags = Assimp.PostProcessSteps;
 namespace Bivrost.Bivrost360Player
 {
 
-    internal class Model3d : IDisposable
+    internal class ModelAsset : IDisposable
     {
 
         protected d3d11.Buffer vertices;
         protected d3d11.Buffer indices;
         protected int verticesCount;
-        protected d3d11.VertexBufferBinding vertexBufferBinding;
-        const int shaderChannelLength = 13;
+        protected int indexCount;
+        protected d3d11.VertexBufferBinding vertexBufferBinding;    // TODO: This might be recreated each frame as in samples
+        const int shaderChannelLength = 13; // TODO: from InputLayout
 
 
-        public Model3d(d3d11.Device device, string objFile)
+        public ModelAsset(d3d11.Device device, string objFile)
         {
             Load(device, objFile);
         }
@@ -32,17 +33,20 @@ namespace Bivrost.Bivrost360Player
             Assimp.Scene scene;
             using (var ctx = new Assimp.AssimpContext())
             {
-                scene = ctx.ImportFile(objFile, assFlags.MakeLeftHanded | assFlags.GenerateUVCoords | assFlags.GenerateNormals);
-                Assimp.Mesh mesh = scene.Meshes[0];
+                scene = ctx.ImportFile(objFile, assFlags.MakeLeftHanded);
+                Assimp.Mesh mesh = scene.Meshes[0];     // TODO: multi-material meshes
 
-                float[] indicesList = new float[mesh.FaceCount * 3];
+                int[] indicesList = new int[mesh.FaceCount * 3];
                 for (int i = 0; i < mesh.FaceCount; i++)
                 {
                     var face = mesh.Faces[i];
-                    if (face.IndexCount != 3) throw new FormatException("Supports only objects with 3-vertex faces");
-                    indicesList[i * 3] = face.Indices[0];
-                    indicesList[i * 3 + 1] = face.Indices[1];
-                    indicesList[i * 3 + 2] = face.Indices[2];
+                    if (face.IndexCount != 3) throw new Exception("Supports only objects with 3-vertex faces");
+                    for (int v = 0; v < 3; v++)
+                    {
+                        int vert = face.Indices[v];
+                        //vert = vert / 3 * shaderChannelLength + vert % 3;    // match input layout
+                        indicesList[i * 3 + v] = vert;
+                    }
                 }
 
                 float[] verticesList = new float[mesh.VertexCount * shaderChannelLength];
@@ -76,7 +80,7 @@ namespace Bivrost.Bivrost360Player
                     }
 
                     float nx = 0;
-                    float ny = 0;
+                    float ny = 1;
                     float nz = 0;
                     if(mesh.HasNormals)
                     {
@@ -85,28 +89,30 @@ namespace Bivrost.Bivrost360Player
                         nz = mesh.Normals[i].Z;
                     }
 
-                    verticesList[i * 13 +  0] = x;
-                    verticesList[i * 13 +  1] = y;
-                    verticesList[i * 13 +  2] = z;
-                    verticesList[i * 13 +  3] = w;
-                    verticesList[i * 13 +  4] = r;
-                    verticesList[i * 13 +  5] = g;
-                    verticesList[i * 13 +  6] = b;
-                    verticesList[i * 13 +  7] = a;
-                    verticesList[i * 13 +  8] = nx;
-                    verticesList[i * 13 +  9] = ny;
-                    verticesList[i * 13 + 10] = nz;
-                    verticesList[i * 13 + 11] = u;
-                    verticesList[i * 13 + 12] = v;
+                    // TODO: from InputLayout
+                    verticesList[i * shaderChannelLength +  0] = x;
+                    verticesList[i * shaderChannelLength +  1] = y;
+                    verticesList[i * shaderChannelLength +  2] = z;
+                    verticesList[i * shaderChannelLength +  3] = w;
+                    verticesList[i * shaderChannelLength +  4] = r;
+                    verticesList[i * shaderChannelLength +  5] = g;
+                    verticesList[i * shaderChannelLength +  6] = b;
+                    verticesList[i * shaderChannelLength +  7] = a;
+                    verticesList[i * shaderChannelLength +  8] = nx;
+                    verticesList[i * shaderChannelLength +  9] = ny;
+                    verticesList[i * shaderChannelLength + 10] = nz;
+                    verticesList[i * shaderChannelLength + 11] = u;
+                    verticesList[i * shaderChannelLength + 12] = 1-v;
                 }
 
                 // Instantiate Vertex buffer from vertex data
                 vertices = d3d11.Buffer.Create(device, d3d11.BindFlags.VertexBuffer, verticesList);
                 verticesCount = verticesList.Length;
                 indices = d3d11.Buffer.Create(device, d3d11.BindFlags.IndexBuffer, indicesList);
+                indexCount = indicesList.Length;
             }
 
-            vertexBufferBinding = new d3d11.VertexBufferBinding(vertices, 13 * sizeof(float), 0);
+            vertexBufferBinding = new d3d11.VertexBufferBinding(vertices, shaderChannelLength * sizeof(float), 0);
         }
 
 
@@ -121,14 +127,12 @@ namespace Bivrost.Bivrost360Player
 
         public void Render(d3d11.DeviceContext context)
         {
-            // Prepare All the stages
             context.InputAssembler.SetVertexBuffers(0, vertexBufferBinding);
             context.InputAssembler.SetIndexBuffer(indices, dxgi.Format.R32_UInt, 0);
 
             context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
-
-            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
-            context.Draw(verticesCount, 0);
+            context.DrawIndexed(indexCount, 0, 0);
+            //context.Draw(verticesCount, 0);
         }
 
         public void Dispose()
@@ -142,14 +146,14 @@ namespace Bivrost.Bivrost360Player
 
     internal class Object3d:IDisposable
     {
-        protected Material material;
-        protected Model3d model;
+        protected MaterialAsset material;
+        protected ModelAsset model;
         protected d3d11.Buffer constantBuffer;
 
         public Matrix World { set; get; }
 
 
-        public Object3d(d3d11.Device device, Model3d model, Material mat)
+        public Object3d(d3d11.Device device, ModelAsset model, MaterialAsset mat)
         {
             this.material = mat;
             this.model = model;
@@ -186,17 +190,18 @@ namespace Bivrost.Bivrost360Player
 
 
 
-    internal class Material : IDisposable
+
+    internal class MaterialAsset : IDisposable
     {
         private d3d11.VertexShader vertexShader;
         private d3d11.PixelShader pixelShader;
         private d3d11.InputLayout layout;
 
-        public Material(d3d11.Device device, string shaderFile):this(device, System.IO.File.ReadAllBytes(shaderFile), shaderFile)
+        public MaterialAsset(d3d11.Device device, string shaderFile):this(device, System.IO.File.ReadAllBytes(shaderFile), shaderFile)
         { }
 
 
-        public Material(d3d11.Device device, byte[] shaderBytes, string shaderName = "unknown")  // TODO: additional shader flags?
+        public MaterialAsset(d3d11.Device device, byte[] shaderBytes, string shaderName = "unknown")  // TODO: additional shader flags?
         {
             // Compile Vertex and Pixel shaders
             using (var vertexShaderByteCode = ShaderBytecode.Compile(shaderBytes, "VS", "vs_4_0", ShaderFlags.None, EffectFlags.None, shaderName))
@@ -241,15 +246,17 @@ namespace Bivrost.Bivrost360Player
     }
 
 
-    internal class SceneSharpDX4 : IScene, IContentUpdatableFromMediaEngine
+    internal partial class SceneSharpDX4 : IScene, IContentUpdatableFromMediaEngine
     {
         private ISceneHost host;
-        private Material mat;
-        Model3d chairModel;
+        private MaterialAsset mat;
+        ModelAsset chairModel;
         Object3d chair1;
         Object3d chair2;
-        Model3d sphereModel;
+        ModelAsset sphereModel;
         Object3d sphere;
+        TextureAsset chairTextureDiffuse;
+        TextureAsset chairTextureNormal;
         private Matrix viewProj;
 
         public SceneSharpDX4(Func<IContentUpdatableFromMediaEngine, bool> contentRequested)
@@ -267,17 +274,19 @@ namespace Bivrost.Bivrost360Player
             this.host = host;
             MediaDecoder.Instance.OnContentChanged += Instance_OnContentChanged;
 
-            mat = new Material(device, "Renderers/MiniTri.fx");
+            mat = new MaterialAsset(device, "Renderers/MiniTri.fx");
 
-            chairModel = new Model3d(device, "Renderers/office_chair.obj");
-            sphereModel = new Model3d(device, "Renderers/sphere.obj");
+            chairModel = new ModelAsset(device, "Renderers/office_chair.obj");
+            sphereModel = new ModelAsset(device, "Renderers/sphere.obj");
 
             chair1 = new Object3d(device, chairModel, mat);
             chair2 = new Object3d(device, chairModel, mat);
             sphere = new Object3d(device, sphereModel, mat);
+
+            chairTextureDiffuse = new TextureAsset(device, "Renderers/office_chair_d.png");
+            chairTextureNormal = new TextureAsset(device, "Renderers/office_chair_n.png");
         }
 
-     
         void IScene.Detach()
         {
             MediaDecoder.Instance.OnContentChanged -= Instance_OnContentChanged;
@@ -300,10 +309,15 @@ namespace Bivrost.Bivrost360Player
 
             context.PixelShader.SetShaderResource(0, textureView);
             context.PixelShader.SetSampler(0, sampler);
+            sphere.Render(viewProj, context);
 
+            context.PixelShader.SetShaderResource(0, chairTextureDiffuse.TextureView);
+            context.PixelShader.SetShaderResource(1, chairTextureNormal.TextureView);
+            context.PixelShader.SetSampler(0, sampler);
+            context.PixelShader.SetSampler(1, sampler);
             chair1.Render(viewProj, context);
             chair2.Render(viewProj, context);
-            sphere.Render(viewProj, context);
+
         }
 
         void IScene.Update(TimeSpan timeSpan)
@@ -314,8 +328,8 @@ namespace Bivrost.Bivrost360Player
             var proj = Matrix.PerspectiveFovRH(72f * (float)Math.PI / 180f, 16f / 9f, 0.01f, 100.0f);
             viewProj = Matrix.Multiply(view, proj);
             chair1.World = Matrix.RotationZ(time * 2) * Matrix.RotationX(time) * Matrix.Translation(Vector3.ForwardRH * 5) * Matrix.Translation(Vector3.Left * 2);
-            chair2.World = Matrix.RotationZ(time * 2) * Matrix.RotationX(time) * Matrix.Translation(Vector3.ForwardRH * 5) * Matrix.Translation(Vector3.Right * 2);
-            sphere.World = Matrix.RotationZ(time / 5) * Matrix.RotationX(time / 3) * Matrix.Scaling(50);
+            chair2.World = Matrix.RotationY(time /2 ) * Matrix.Translation(Vector3.ForwardRH * 5) * Matrix.Translation(Vector3.Right * 2);
+            sphere.World = Matrix.RotationY(time / 5) /** Matrix.RotationX(time / 3) */ * Matrix.Scaling(50);
         }
 
         #region content updates
---
+++ 360Player/Renderers/TextureAsset.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using SharpDX;
+using d3d11 = SharpDX.Direct3D11;
+
+namespace Bivrost.Bivrost360Player
+{
+
+
+    internal partial class SceneSharpDX4
+    {
+        internal class TextureAsset:IDisposable
+        {
+            public d3d11.Texture2D Texture { get; protected set; }
+            public d3d11.ShaderResourceView TextureView { get; protected set; }
+
+            public TextureAsset(d3d11.Device device, string path)
+            {
+                using (var image = Image.FromFile(path))
+                using (var bitmap = new Bitmap(image))
+                {
+                    Bitmap bitmapArgb32;
+                    if (image.PixelFormat != PixelFormat.Format32bppArgb)
+                    {
+                        bitmapArgb32 = bitmap.Clone(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height), PixelFormat.Format32bppArgb);
+                    }
+                    else 
+                    {
+                        bitmapArgb32 = bitmap;
+                    }
+
+                    var data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                    Texture = new d3d11.Texture2D(device, new d3d11.Texture2DDescription()
+                    {
+                        Width = bitmap.Width,
+                        Height = bitmap.Height,
+                        ArraySize = 1,
+                        BindFlags = d3d11.BindFlags.ShaderResource,
+                        Usage = d3d11.ResourceUsage.Immutable,
+                        CpuAccessFlags = d3d11.CpuAccessFlags.None,
+                        Format = SharpDX.DXGI.Format.B8G8R8A8_UNorm,
+                        MipLevels = 1,
+                        OptionFlags = d3d11.ResourceOptionFlags.None,
+                        SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0),
+                    }, new DataRectangle(data.Scan0, data.Stride));
+                    bitmap.UnlockBits(data);
+
+                    if(bitmap != bitmapArgb32)
+                    {
+                        bitmapArgb32.Dispose();
+                    }
+                }
+
+                TextureView = new d3d11.ShaderResourceView(device, Texture);
+            }
+
+            public void Dispose()
+            {
+                Texture.Dispose();
+                TextureView.Dispose();
+            }
+            
+        }
+    }
+}
